225c75039d0a949bfedc4381f3af58ca4bbe7e7f
Glauber-0016 <20232ireads0016@ifba.edu.br>
2024-11-10 15:46:34 -0300

    Atividade 07
    Atividade 07, criando classe fatorial

---
+++ src/br/com/ifba/atividade07/view/Fatorial.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.atividade07.view;
+
+/**
+ *
+ * @author Glauber
+ */
+public class Fatorial {
+    private int valor;
+    
+    public void setValor (int n) {
+       this.valor = n;
+    }
+    
+    //Pegando o Fatorial de um Número n
+    public int getFatorial (int n) {
+        int fatorial = 1;
+          
+        for (int i = valor; i > 1; i--)
+        {
+            fatorial *= i;
+        }
+        
+        return fatorial;
+    }
+    
+    public String getFormula () {
+        StringBuilder formula = new StringBuilder();
+        
+        for (int i = valor; i > 1; i--) {
+            formula.append(i).append(" * ");
+        }
+        formula.append("1");
+
+        return formula.toString();
+ 
+    }
+    
+}
